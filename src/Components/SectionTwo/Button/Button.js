277a50b9f7716e42c8641f09ee5159d6ec84dd0b
leoarthurwall <leowall93@gmail.com>
2022-11-13 10:28:46 +0000

    created button component for section 2

---
+++ src/Components/SectionTwo/Button/Button.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const ButtonContainer = styled.button`
+ width: 300px;
+ height: 90px
+ color: #fff;
+ outline: none;
+ border: none;
+
+`
+
+const Button = () => {
+  return (
+    <ButtonContainer>Button</ButtonContainer>
+  )
+}
+
+export default Button
