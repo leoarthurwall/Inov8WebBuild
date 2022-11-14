a1911e82d2365ebe47b2321da00114e524061c02
leoarthurwall <leowall93@gmail.com>
2022-11-14 21:56:48 +0100

    put button in global component folder as it is reusable in 2 sections

--- src/Components/SectionFive/SectionFive.js
+++ src/Components/SectionFive/SectionFive.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Button from "../Global/Button/Button";
 
 const SectionFiveContainer = styled.section`
-    height: 100vh;
-    width: 100%;
-    background: #fff;
-`
-
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
 const SectionFive = () => {
   return (
-    <SectionFiveContainer>SectionFive</SectionFiveContainer>
-  )
-}
+    <SectionFiveContainer>
+      <ButtonContainer>
+        <Button buttonText={"SHOP MEN"} />
+        <Button buttonText={"SHOP WOMEN"} />
+      </ButtonContainer>
+    </SectionFiveContainer>
+  );
+};
 
-export default SectionFive
+export default SectionFive;
--- src/Components/SectionTwo/SectionTwo.js
+++ src/Components/SectionTwo/SectionTwo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Button from "./Button/Button";
+import Button from "../Global/Button/Button";
 //SECTION - PARKCLAW
 
 const SectionTwoContainer = styled.section`
