d0559ea918167412bff150c3f733e1923d84bf16
leoarthurwall <leowall93@gmail.com>
2022-11-14 22:38:46 +0100

    added a text component for extion two. set up boilerplate component code

--- src/Components/Global/Button/Button.js
+++ src/Components/Global/Button/Button.js
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 
 const ButtonContainer = styled.button`
- width: 300px;
- height: 90px;
- background-color:${props => props.bgColor};
- outline: none;
- border: none;
- color: ${props => props.color};
- font-weight: bold;
-`
+  width: 300px;
+  height: 90px;
+  background-color: ${(props) => props.bgColor};
+  outline: none;
+  border: none;
+  color: ${(props) => props.color};
+  font-weight: bold;
+`;
 
 const Button = ({ buttonText, bgColor, color }) => {
-  return <ButtonContainer color={color} bgColor={bgColor}>{buttonText}</ButtonContainer>;
+  return (
+    <ButtonContainer color={color} bgColor={bgColor}>
+      {buttonText}
+    </ButtonContainer>
+  );
 };
 
 export default Button;
---
+++ src/Components/SectionTwo/SectionTwoText/SectionTwoText.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TextContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: center;
+`
+
+const SectionTwoText = () => {
+  return (
+    <TextContainer>SectionTwoText</TextContainer>
+  )
+}
+
+export default SectionTwoText
