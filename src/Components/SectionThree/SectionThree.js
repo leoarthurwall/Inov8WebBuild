660d1d4e7c109baba5e36c5fbd358c08f1007b56
leoarthurwall <leowall93@gmail.com>
2022-11-15 18:21:41 +0100

    created an arraw with the grid images, and containers to wrap them - section 3

--- src/Components/SectionThree/SectionThree.js
+++ src/Components/SectionThree/SectionThree.js
@@ -1,16 +1,51 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const SectionThreeContainer = styled.section`
-    height: 100vh;
-    width: 100%;
-    background: #fff;
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+`;
+const ImageWrapper = styled.div`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+  justify-content: center;
+  flex: wrap;
+  align-items: center;
+`;
+const Image = styled.div`
+  height: 50%;
+  width: 50%;
 `
 
 const SectionThree = () => {
-  return (
-    <SectionThreeContainer>SectionThree</SectionThreeContainer>
-  )
-}
+  const ImagePath = [
+    {
+      name: "grid-1",
+      bgImage: "/assets/desktop/grid-1.jpg",
+    },
+    {
+      name: "grid-2",
+      bgImage: "/assets/desktop/grid-2.jpg",
+    },
+    {
+      name: "grid-3",
+      bgImage: "/assets/desktop/grid-3.jpg",
+    },
+    {
+      name: "grid-4",
+      bgImage: "/assets/desktop/grid-4.jpg",
+    }
+  ]
 
-export default SectionThree
+  return (
+    <SectionThreeContainer>
+      <ImageWrapper></ImageWrapper>
+    </SectionThreeContainer>
+  );
+};
+
+export default SectionThree;
