f4a14351cfedc24329bc49b827bc889a725b8a97
leoarthurwall <leowall93@gmail.com>
2022-11-15 21:06:53 +0100

    section 7 - set up the placeholder grid template

--- src/Components/SectionSeven/SectionSeven.js
+++ src/Components/SectionSeven/SectionSeven.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const SectionSevenContainer = styled.section`
-    height: 100vh;
-    width: 100%;
-    background: #fff;
-`
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+`;
 const Header = styled.h2`
   color: #000000;
   font-size: 50px;
@@ -14,18 +14,39 @@ const Header = styled.h2`
   text-align: left;
   padding: 50px;
   box-sizing: border-box;
+`;
 
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-template-rows: 300px auto;
+  grid-gap: 10px;
+  background-color: grey;
+`;
+
+const Image = styled.div`
+  background-color: blue;
 
 `
+const Text = styled.h4`
+  background-color: white;
 
-
+`
 
 const SectionSeven = () => {
   return (
     <SectionSevenContainer>
-    <Header>PARKCLAW G 280 STORIES</Header>
+      <Header>PARKCLAW G 280 STORIES</Header>
+      <GridContainer>
+        <Image />
+        <Image />
+        <Image />
+        <Text />
+        <Text />
+        <Text />
+      </GridContainer>
     </SectionSevenContainer>
-  )
-}
+  );
+};
 
-export default SectionSeven
+export default SectionSeven;
