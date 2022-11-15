fccb1159565279ca117eef3bf1ba797924b08e81
leoarthurwall <leowall93@gmail.com>
2022-11-15 01:14:48 +0100

    added all styled components to the notifications section on the page, first wave of basic styling complete

--- src/Components/SectionOne/SectionOneNotification/SectionOneNotification.js
+++ src/Components/SectionOne/SectionOneNotification/SectionOneNotification.js
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import {AiOutlineCloseCircle} from 'react-icons/ai'
-import {BsCalendar3} from 'react-icons/bs'
-
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { BsCalendar3 } from "react-icons/bs";
 
 const NotificationContainer = styled.div`
   top: 71px;
@@ -23,21 +22,31 @@ const NotificationContainer = styled.div`
 `;
 
 const TopRowContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const TopRowLeft = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 1rem;
+`;
+const ReminderText = styled.h4`
+  color: #fff;
+  margin: 0;
+  font-size: 20px;
+`;
 const TextContainer = styled.div`
-display:flex;
-flex-direction: column;
-gap: 12px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
 const NotificationText = styled.h3`
   color: #fff;
   font-weight: 600;
   font-family: sans-serif;
-  font-size: 54px;
+  font-size: 64px;
   margin: 0;
   height: auto;
 `;
@@ -45,10 +54,13 @@ const NotificationText = styled.h3`
 const SectionOneNotification = () => {
   return (
     <NotificationContainer>
-    <TopRowContainer>
-    <BsCalendar3 />
-    <AiOutlineCloseCircle />
-    </TopRowContainer>
+      <TopRowContainer>
+        <TopRowLeft>
+          <BsCalendar3 size={40} color="#fff" />
+          <ReminderText>REMINDER</ReminderText>
+        </TopRowLeft>
+        <AiOutlineCloseCircle size={40} color="#fff" />
+      </TopRowContainer>
       <TextContainer>
         <NotificationText>6AM</NotificationText>
         <NotificationText>TEAMS MEETING</NotificationText>
