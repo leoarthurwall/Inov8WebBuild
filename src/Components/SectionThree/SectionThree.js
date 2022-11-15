598a1324117c138e5749fcba806fcac9a81094d0
leoarthurwall <leowall93@gmail.com>
2022-11-15 20:07:07 +0100

    section 3 - used flex grid along with position relative and absolute to create a grid effect that overlaps the above and below sections

--- src/Components/SectionThree/SectionThree.js
+++ src/Components/SectionThree/SectionThree.js
@@ -2,58 +2,71 @@ import React from "react";
 import styled from "styled-components";
 
 const SectionThreeContainer = styled.section`
-  height: 100vh;
+  height: 1053px;
   width: 100%;
   background: #fff;
-`;
-const ImageWrapper = styled.div`
-  max-width: 1200px;
-  width: 1200px;
-  margin-left: auto;
-  margin-right: auto;
-
+  position: relative;
   display: flex;
   justify-content: center;
-  flex: wrap;
   align-items: center;
+`;
+const ImageWrapper = styled.div`
+  width: 1200px;
+  height: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 20px;
   background-color: grey;
 `;
 const Image = styled.div`
-  height: 50%;
-  width: 50%;
+  height: auto;
+  width: auto;
+  background-color: blue;
 `;
 
 const SectionThree = () => {
-  const ImagePaths = [
-    {
-      name: "grid-1",
-      bgImage: "/assets/desktop/grid-1.jpg",
-    },
-    {
-      name: "grid-2",
-      bgImage: "/assets/desktop/grid-2.jpg",
-    },
-    {
-      name: "grid-3",
-      bgImage: "/assets/desktop/grid-3.jpg",
-    },
-    {
-      name: "grid-4",
-      bgImage: "/assets/desktop/grid-4.jpg",
-    },
-  ];
+  // const ImagePaths = [
+  //   {
+  //     name: "grid-1",
+  //     bgImage: "/assets/desktop/grid-1.jpg",
+  //   },
+  //   {
+  //     name: "grid-2",
+  //     bgImage: "/assets/desktop/grid-2.jpg",
+  //   },
+  //   {
+  //     name: "grid-3",
+  //     bgImage: "/assets/desktop/grid-3.jpg",
+  //   },
+  //   {
+  //     name: "grid-4",
+  //     bgImage: "/assets/desktop/grid-4.jpg",
+  //   },
+  // ];
 
   return (
     <SectionThreeContainer>
       <ImageWrapper>
-        {ImagePaths.map((imagePath, i) => {
+        {/* {ImagePaths.map((imagePath, i) => {
           return (
             <Image
               key={i}
               style={{ backgroundImage: `url(${imagePath.bgImage})` }}
             />
           );
-        })}
+        })} */}
+        <Image />
+        <Image />
+        <Image />
+        <Image />
       </ImageWrapper>
     </SectionThreeContainer>
   );
