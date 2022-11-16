9e31b6dc71af5ea79d2ac154750291007760a0cd
leoarthurwall <leowall93@gmail.com>
2022-11-16 19:12:14 +0100

    section - 3 set a max width of the image so it strinks with screen size below 1280px

--- src/Components/SectionThree/SectionThree.js
+++ src/Components/SectionThree/SectionThree.js
@@ -9,25 +9,19 @@ const SectionThreeContainer = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-
 `;
 const ImageWrapper = styled.div`
- 
-  margin-left: auto;
-  margin-right: auto;
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+
   z-index: 100;
 
   display: grid;
   grid-template-columns: auto auto;
   grid-gap: 1vw;
-
 `;
 const Image = styled.img`
-  width: 580px;
+  width: 100%;
+  max-width: 580px;
 `;
 
 const SectionThree = () => {
